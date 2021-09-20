e212a7c0d5931133a81639f6cf4ae73f37348bbf
--local <miloszweb@gmail.com>
2021-09-20 16:28:20 +0200

    added group and user entities, modified repositories method names to more accurate

--- src/repositories/groupRepository.py
+++ src/repositories/groupRepository.py
@@ -1,3 +1,5 @@
+from ..entities.group import Group
+from ..entities.user import User
 from ..firebaseProxy import FirebaseProxy
 
 
@@ -7,12 +9,40 @@ class GroupRepository():
     def __init__(self) -> None:
         self.firebase = FirebaseProxy()
 
-    def get(self, id: int) -> dict:
-        result = []
-        groupData = self.firebase.getChilds(FirebaseProxy.group_index, id).get()
-        
-        if groupData.each():
-            for user_root in groupData.each():
-                result.append(user_root.val())
+    def get(self, id: int) -> Group:
+        group = Group(id)
+        fbData = self.firebase.getChilds(FirebaseProxy.group_index, id).get()
+        users = []
 
-        return result
+        for userData in fbData.each() or []:
+            userData = userData.val()
+            users.append(
+                User(
+                    userData.get(FirebaseProxy.id_index),
+                    userData.get(FirebaseProxy.name_index)
+                )
+            )
+    
+        group.setUsers(users)
+
+        return group
+
+    def save(self, group: Group) -> None:
+        users = {}
+
+        if not group.getUsers():
+            self.remove(group)
+
+        for user in group.getUsers():
+            users[user.getId()] = {
+                FirebaseProxy.id_index: user.getId(),
+                FirebaseProxy.name_index: user.getUsername()
+            }
+        
+        self.firebase.getChilds(
+            FirebaseProxy.group_index,
+            group.getId()
+        ).update(users)
+
+    def remove(self, group: Group) -> None:
+        self.firebase.getChilds(FirebaseProxy.group_index, group.getId()).remove()
