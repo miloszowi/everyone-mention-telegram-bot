73a2cc6698c465e5865dbac2104ef0482d36616a
miloszowi <miloszweb@gmail.com>
2021-09-22 19:53:46 +0200

    webhook to start pooling, removed env variables for app url and port
    Author:    miloszowi <miloszweb@gmail.com>
    
    Author:    miloszowi <miloszweb@gmail.com>

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from .config.credentials import bot_token, app_url, port
+from .config.credentials import bot_token
 from .config.handlers import handlers
 from .handlers.handlerInterface import HandlerInterface
 from telegram.ext.dispatcher import Dispatcher
@@ -14,8 +14,7 @@ class App:
 
     def run(self) -> None:
         self.registerHandlers()
-        self.registerWebhook()
-
+        self.updater.start_polling()
         self.updater.idle()
 
     def registerHandlers(self) -> None:
@@ -24,11 +23,3 @@ class App:
                 raise Exception('Invalid list of handlers provided. Handler must implement HandlerInterface')
 
             self.updater.dispatcher.add_handler(handler.getBotHandler())
-
-    def registerWebhook(self) -> None:
-        self.updater.start_webhook(
-            listen="0.0.0.0",
-            port=int(port),
-            url_path=bot_token,
-            webhook_url=f'{app_url}/{bot_token}'
-        )
